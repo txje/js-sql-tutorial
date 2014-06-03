be3815c9474d943d94bdded1ed3a578baf33d3b7
txje <jeremyrwang@gmail.com>
2014-06-03 10:55:16 -0400

    Load made database, simplify tutorial (more manual), table result display

--- tutorial0.sql
+++ tutorial0.sql
@@ -1,3 +1,1 @@
-CREATE TABLE IF NOT EXISTS Person ("name" TEXT, "age" INTEGER, "height" REAL);
-INSERT INTO Person VALUES ("Johnny", 105, 5.4), ("Katy", 78, 5.2);
-SELECT * FROM Person;
+SELECT * FROM Categories LIMIT 1;
